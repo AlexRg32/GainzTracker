e70686736054194d76e1ec510a24b6dac8584cf7
AlexRg32 <alexrg32@outlook.es>
2025-07-01 18:55:23 +0200

    Header actualizado

---
+++ src/app/components/Avatar.tsx
@@ -0,0 +1,13 @@
+
+const Avatar = () => {
+    return(
+        <div className="w-16 h-16 rounded-full overflow-hidden">
+            <img
+                src="https://avatars.githubusercontent.com/u/12345678?v=4"
+                alt="Avatar"
+                className="w-full h-full object-cover"
+            />
+        </div>
+    )
+}
+export default Avatar;
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,31 +1,42 @@
-import Image from "next/image";
-import logo from "@/app/assets/logo.png";
+import { Dumbbell } from "lucide-react";
+import { User } from "lucide-react";
+import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="bg-blue-950 text-white  p-4 fixed top-0 left-0 w-full z-50">
-      <div className="container mx-auto flex justify-between items-center">
-        <Image
-          src={logo}
-          alt="Logo de GainzTracker"
-          className="w-20 h-auto"
-          priority
-        />
-       <nav className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
-          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
-            <li>
-              <a href="#track" className="hover:text-indigo-300">
-                Trackear Entrenamiento
-              </a>
-            </li>
-            <li>
-              <a href="#calculator" className="hover:text-indigo-300">
-                Calcular 1RM
-              </a>
-            </li>
-          </ul>
-        </nav>
+    <header className="bg-blue-950 text-white  p-4 fixed top-0 left-0 w-full h-20 z-50 ">
+
+      <div className="  flex flex-row md:flex-row justify-between items-center h-full   ">
+
+        <div className=" flex flex-row space-x-4 items-center md:ml-6 ">
+          <Dumbbell className="w-10 h-10 text-indigo-300"></Dumbbell>
+          <h1 className="text-2xl font-bold text-indigo-300">Gainz Tracker</h1>
+        </div>
+
+        <div className=" flex items-center md:mr-6 ">
+
+          <nav className=" space-y-2 md:space-y-0 md:space-x-6">
+            <ul className=" space-y-2 space-x-2 md:space-y-0 md:space-x-7 flex flex-row md:flex-row items-center">
+              <li className="hidden md:block">
+                <a href="#track" className="hover:text-indigo-300">
+                  Calculadora
+                </a>
+              </li>
+              <li className="block md:hidden">
+                <Menu className=""></Menu>
+              </li>
+              <li>
+                <button>
+                  <User></User>
+                </button>
+              </li>
+            </ul>
+          </nav>
+
+        </div>
+
       </div>
+
     </header>
   );
 };
